b0dcd2802bdf1b8b6bb533fb9eaf9d80da3f9b38
Ana Canto <ana.lucia.canto@poli.ufrj.br>
2022-07-24 15:28:57 -0300

    Adding card

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,7 +16,3 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-
-root.render(
- 
-);
--- frontend/src/pages/Admin/index.js
+++ frontend/src/pages/Admin/index.js
@@ -1,8 +1,33 @@
 import React from "react";
+import Card from "../../components/Card"
 
 const Admin = () => {
+    const data = React.useMemo(
+        () => [            
+            {   id: 1,
+                name: "Local A",
+                measures: {
+                     temperature: 35,
+                     humidity: 60
+                }                
+            },
+            {   id: 2,
+                name: "Local B",
+                measures: {
+                     temperature: 38,
+                     humidity: 55
+                }                
+            }                  
+        ],
+        []
+      )
+
    return(
-    <div> Página admin</div>     
+    <div>
+        {data.map((material)=> (
+            <Card key={material.id} material={material}  />
+        ))}
+    </div>     
     )
 }
 
